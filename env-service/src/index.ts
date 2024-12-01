86d7878c5ad999be83ab2244db0056eeed20111a
Guru901 <avinashkaur842@gmail.com>
2024-12-01 10:23:42 +0000

    env service tRPC setup

--- env-service/src/index.ts
+++ env-service/src/index.ts
@@ -1,16 +1,21 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { appRouter } from "./trpc.js";
+import { trpcServer } from "@hono/trpc-server";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(
+  "/trpc/*",
+  trpcServer({
+    router: appRouter,
+  })
+);
 
-const port = 3000
-console.log(`Server is running on http://localhost:${port}`)
+const port = 3000;
+console.log(`Server is running on http://localhost:${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
